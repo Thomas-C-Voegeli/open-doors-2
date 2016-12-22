8676f8f05521e317a470ea5446e27102b311eb70
Thomas-C-Voegeli <tcvoegeli@gmail.com>
2016-12-22 12:04:07 -0800

    Add spec for User and Door model validations

--- test/models/door_test.rb
+++ test/models/door_test.rb
@@ -1,7 +1,84 @@
 require 'test_helper'
 
 class DoorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  RSpec.describe User do
+
+    context "when posting required fields" do
+      it "Creates a new door" do
+        test_door_1 = Door.new(
+        						country: "U.S.",
+        						state: "IL",
+        						city: "Chicago",
+        						street: "W. Adams St.",
+        						street_number: "233",
+        						zipcode: "54233"
+                              )
+        expect(test_door_1.save).to eq true
+      end
+    end
+
+    context "when omitting required fields" do
+      it "Doesn't save to database when country is omitted" do
+        test_door_2 = User.new(country: "",
+        						state: "IL",
+        						city: "Chicago",
+        						street: "W. Adams St.",
+        						street_number: "233",
+        						zipcode: "54233"
+                              )
+        expect(test_user_2.save).to eq false
+      end
+
+      it "Doesn't save to database when state is omitted" do
+        test_door_3 = User.new(country: "U.S.",
+        						state: "",
+        						city: "Chicago",
+        						street: "W. Adams St.",
+        						street_number: "233",
+        						zipcode: "54233"
+                              )
+        expect(test_door_3.save).to eq false
+      end
+      it "Doesn't save to database when city is omitted" do
+        test_door_4 = User.new(country: "U.S.",
+        						state: "IL",
+        						city: "",
+        						street: "W. Adams St.",
+        						street_number: "233",
+        						zipcode: "54233"
+                              )
+        expect(test_door_4.save).to eq false
+      end
+      it "Doesn't save to database when street is omitted" do
+        test_door_5 = User.new(country: "U.S.",
+        						state: "IL",
+        						city: "Chicago",
+        						street: "",
+        						street_number: "233",
+        						zipcode: "54233"
+                              )
+        expect(test_door_5.save).to eq false
+      end
+      it "Doesn't save to database when street number is omitted" do
+        test_door_6 = User.new(country: "U.S.",
+        						state: "IL",
+        						city: "Chicago",
+        						street: "W. Adams St.",
+        						street_number: "",
+        						zipcode: "54233"
+                              )
+        expect(test_door_6.save).to eq false
+      end
+       it "Doesn't save to database when zipcode is omitted" do
+        test_door_7 = User.new(country: "U.S.",
+        						state: "IL",
+        						city: "Chicago",
+        						street: "W. Adams St.",
+        						street_number: "233",
+        						zipcode: ""
+                              )
+        expect(test_door_7.save).to eq false
+      end
+    end
+
 end

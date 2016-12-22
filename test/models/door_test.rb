d1d940da3f99460484f92dc0ab37f3e1d7cc9309
Thomas-C-Voegeli <tcvoegeli@gmail.com>
2016-12-22 12:21:35 -0800

    Fix more typos

--- test/models/door_test.rb
+++ test/models/door_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class DoorTest < ActiveSupport::TestCase
-  RSpec.describe User do
+  RSpec.describe Door do
 
     context "when posting required fields" do
       it "Creates a new door" do
@@ -19,7 +19,7 @@ class DoorTest < ActiveSupport::TestCase
 
     context "when omitting required fields" do
       it "Doesn't save to database when country is omitted" do
-        test_door_2 = User.new(country: "",
+        test_door_2 = Door.new(country: "",
         						state: "IL",
         						city: "Chicago",
         						street: "W. Adams St.",
@@ -30,7 +30,7 @@ class DoorTest < ActiveSupport::TestCase
       end
 
       it "Doesn't save to database when state is omitted" do
-        test_door_3 = User.new(country: "U.S.",
+        test_door_3 = Door.new(country: "U.S.",
         						state: "",
         						city: "Chicago",
         						street: "W. Adams St.",
@@ -40,7 +40,7 @@ class DoorTest < ActiveSupport::TestCase
         expect(test_door_3.save).to eq false
       end
       it "Doesn't save to database when city is omitted" do
-        test_door_4 = User.new(country: "U.S.",
+        test_door_4 = Door.new(country: "U.S.",
         						state: "IL",
         						city: "",
         						street: "W. Adams St.",
@@ -50,7 +50,7 @@ class DoorTest < ActiveSupport::TestCase
         expect(test_door_4.save).to eq false
       end
       it "Doesn't save to database when street is omitted" do
-        test_door_5 = User.new(country: "U.S.",
+        test_door_5 = Door.new(country: "U.S.",
         						state: "IL",
         						city: "Chicago",
         						street: "",
@@ -60,7 +60,7 @@ class DoorTest < ActiveSupport::TestCase
         expect(test_door_5.save).to eq false
       end
       it "Doesn't save to database when street number is omitted" do
-        test_door_6 = User.new(country: "U.S.",
+        test_door_6 = Door.new(country: "U.S.",
         						state: "IL",
         						city: "Chicago",
         						street: "W. Adams St.",
@@ -70,7 +70,7 @@ class DoorTest < ActiveSupport::TestCase
         expect(test_door_6.save).to eq false
       end
        it "Doesn't save to database when zipcode is omitted" do
-        test_door_7 = User.new(country: "U.S.",
+        test_door_7 = Door.new(country: "U.S.",
         						state: "IL",
         						city: "Chicago",
         						street: "W. Adams St.",
